b8662119112b2e3f128f2553eeeeb21ceb611464
LukasDD82 <lukas.simek.de@gmail.com>
2025-05-16 21:28:48 +0200

    Hoste

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Guest>actualGuest= new ArrayList<>();
+
+        Guest adelaMalikova
+                = new Guest("Adela", "Malikova", LocalDate.of(1993,3,3));
+
+
+        Guest janDvoracek
+                = new Guest("Jan", "Dvoracek", LocalDate.of(1995,5,5));
+
+        janDvoracek.setBirthDate(LocalDate.of(1995,4,5));
+
+        actualGuest.add(adelaMalikova);
+        actualGuest.add(janDvoracek);
+
+
+        List<Room>occupiedRooms= new ArrayList<>();
+
+        }
+    }
---
+++ src/Room.java
@@ -0,0 +1,90 @@
+import java.time.LocalDateTime;
+
+public class Room {
+
+    private int roomNumber;
+    private int numberOfBeds;
+    private Boolean balcony;
+    private Boolean seaView;
+    private double price;
+    private LocalDateTime checkIn;
+    private LocalDateTime checkOut;
+    private Boolean isWorkingStay;
+
+    public Room(int roomNumber, Boolean balcony, int numberOfBeds, Boolean seaView, double price, LocalDateTime checkOut, LocalDateTime checkIn, Boolean isWorkingStay) {
+        this.roomNumber = roomNumber;
+        this.balcony = balcony;
+        this.numberOfBeds = numberOfBeds;
+        this.seaView = seaView;
+        this.price = price;
+        this.checkOut = checkOut;
+        this.checkIn = checkIn;
+        this.isWorkingStay = isWorkingStay;
+
+
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public void setNumberOfBeds(int numberOfBeds) {
+        this.numberOfBeds = numberOfBeds;
+    }
+
+    public Boolean getBalcony() {
+        return balcony;
+    }
+
+    public void setBalcony(Boolean balcony) {
+        this.balcony = balcony;
+    }
+
+    public Boolean getSeaView() {
+        return seaView;
+    }
+
+    public void setSeaView(Boolean seaView) {
+        this.seaView = seaView;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(LocalDateTime checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public LocalDateTime getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(LocalDateTime checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public Boolean getWorkingStay() {
+        return isWorkingStay;
+    }
+
+    public void setWorkingStay(Boolean workingStay) {
+        isWorkingStay = workingStay;
+    }
+}
