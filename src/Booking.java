dabf5b3021c5f30d996bda57c60ecd7e88459961
LukasDD82 <lukas.simek.de@gmail.com>
2025-05-17 15:55:15 +0200

    Guest+Rooms+Guest

--- src/Booking.java
+++ src/Booking.java
@@ -1,7 +1,51 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Booking {
     private Room room;
     private Guest guest;
     private List otherGuests;
+
+    public Booking(Room room, Guest guest, List otherGuests) {
+        this.room = room;
+        this.guest = guest;
+        this.otherGuests = otherGuests;
+    }
+
+    public void addGuest(Guest guest) {
+        otherGuests.add(guest);
+    }
+
+    public void removeGuest(Guest guest) {
+        otherGuests.remove(guest);
+    }
+
+    public List<Guest> getotherGuests() {
+
+        return new ArrayList<>(otherGuests);
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Guest getGuests() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public List getOtherGuests() {
+        return otherGuests;
+    }
+
+    public void setOtherGuests(List otherGuests) {
+        this.otherGuests = otherGuests;
+    }
 }
